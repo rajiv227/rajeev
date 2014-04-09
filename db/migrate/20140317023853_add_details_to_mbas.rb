1c2f84851cbfc669710ad5cfd94f17d0f5c8f3ca
rajiv227 <rajeev6d@gmail.com>
2014-04-08 22:41:00 -0700

    Delete 20140317023853_add_details_to_mbas.rb

--- db/migrate/20140317023853_add_details_to_mbas.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToMbas < ActiveRecord::Migration
-  def change
-    add_column :mbas, :mba_type, :string
-    add_column :mbas, :company, :string
-    add_column :mbas, :function, :string
-  end
-end
